5cee33d055778efeb7ac75a3916c631a892e9f3c
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-06 15:04:32 +0530

    detail das pls update

--- my-app/src/PlanetInfo.js
+++ my-app/src/PlanetInfo.js
@@ -8,16 +8,30 @@ function PlanetInfo({ planet }) {
             <img src={planet.image} alt={planet.title} />
             <h2>{planet.title}</h2>
             <p>{planet.desc}</p>
-            <p>Distance: {planet.distance.value} {planet.distance.unit}</p>
+            {planet.distance && 
+                <p>Distance: {planet.distance.value} {planet.distance.unit}</p>
+            }
+            
             <h4>Travel Tips:</h4>
             <p>{planet.tips}</p>
+            
             <h4>Available Travel Methods:</h4>
             {planet.travel_methods && 
-    <ul>
-        {planet.travel_methods.map(method => <li key={method}>{method}</li>)}
-    </ul>
-}
+                <ul>
+                    {Object.values(planet.travel_methods).map((method, index) => <li key={index}>{method}</li>)}
+                </ul>
+            }
 
+            <h4>Points of Interest:</h4>
+            <ul>
+                {planet.points_of_interest && planet.points_of_interest.map((poi, index) => (
+                    <li key={index}>
+                        <strong>Name:</strong> {poi.name}<br />
+                        <strong>Description:</strong> {poi.desc}<br />
+                        <img src={poi.image} alt={poi.name} />
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
