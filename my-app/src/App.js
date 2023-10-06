590d868097744b53e58b53275a644637158d6d5c
YoungGuy2003 <abhijithts402@gmail.com>
2023-10-06 11:46:18 +0530

    added solar system

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,29 @@ import Navigation from './Navigation';
 import PlanetInfo from './PlanetInfo';
 import Itinerary from './Itinerary';
 import planetsData from './data/planets.json';
+import './App.css';
 
 function App() {
     const [selectedPlanet, setSelectedPlanet] = React.useState(null);
 
     return (
-        <div>
-            <Navigation onSelectPlanet={setSelectedPlanet} />
-            <PlanetInfo planet={planetsData[selectedPlanet]} />
-            <Itinerary planet={planetsData[selectedPlanet]} />
+        <div className="container">
+            <header className="header">
+                <h1>Explore the Planets</h1>
+            </header>
+            <div className="navigation">
+                <Navigation onSelectPlanet={setSelectedPlanet} />
+            </div>
+            {selectedPlanet && (
+                <div>
+                    <div className="planet-info">
+                        <PlanetInfo planet={planetsData[selectedPlanet]} />
+                    </div>
+                    <div className="itinerary">
+                        <Itinerary planet={planetsData[selectedPlanet]} />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
