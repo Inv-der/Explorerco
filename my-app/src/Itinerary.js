b50a9ebfbc65ccc7386011b34ff3ab565a600281
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-06 15:52:17 +0530

    itinerary

--- my-app/src/Itinerary.js
+++ my-app/src/Itinerary.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import planets from './data/planets.json';
 
 function Itinerary({ planet }) {
     const [selectedPOIs, setSelectedPOIs] = useState([]);
@@ -7,40 +6,58 @@ function Itinerary({ planet }) {
     if (!planet) return <p>Select a planet to plan your itinerary.</p>;
 
     const handlePOISelection = (poi) => {
-        if (!selectedPOIs.some(selectedPOI => selectedPOI.name === poi.name)) {
-          setSelectedPOIs(prev => [...prev, poi]);
+        if (selectedPOIs.some(selectedPOI => selectedPOI.name === poi.name)) {
+            setSelectedPOIs(prev => prev.filter(p => p.name !== poi.name));
+        } else {
+            setSelectedPOIs(prev => [...prev, poi]);
         }
-      };
+    };
+
+    const downloadBucketList = () => {
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(selectedPOIs));
+        const downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href", dataStr);
+        downloadAnchorNode.setAttribute("download", "bucketList.json");
+        document.body.appendChild(downloadAnchorNode); 
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    }
       
-  return (
-    <div>
-      <h3>Plan Your Itinerary for {planet.title}</h3>
+    return (
+        <div>
+            <h3>Plan Your Itinerary for {planet.title}</h3>
 
-      <h4>Select Points of Interest:</h4>
-      {planet.POIs && (
-        <ul>
-          {planet.POIs.map((poi) => (
-            <li key={poi.name}>
-              <button onClick={() => handlePOISelection(poi)}>
-                {poi.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+            <h4>Visitable place:</h4>
+            {planet.points_of_interest && (
+                <ul>
+                    {planet.points_of_interest.map((poi) => (
+                        <li key={poi.name}>
+                            <button onClick={() => handlePOISelection(poi)}>
+                                {selectedPOIs.some(p => p.name === poi.name) ? `Remove ${poi.name}` : poi.name}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
 
-      <h4>Your Itinerary:</h4>
-      <ul>
-        {selectedPOIs.map((poi) => (
-          <li key={poi.name}>
-            <h5>{poi.name}</h5>
-            <p>{poi.desc}</p>
-            <img src={poi.image} alt={poi.name} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <h4>My Bucket list:</h4>
+            <ul>
+                {selectedPOIs.map((poi) => (
+                    <li key={poi.name}>
+                        <h5>{poi.name}</h5>
+                        <p>{poi.desc}</p>
+                        <img src={poi.image} alt={poi.name} />
+                    </li>
+                ))}
+            </ul>
+
+            {selectedPOIs.length > 0 && (
+                <button onClick={downloadBucketList}>
+                    Download Bucket List
+                </button>
+            )}
+        </div>
+    );
 }
 
 export default Itinerary;
---
+++ my-app/src/carousel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Carousel() {
+  return (
+    <div id="carouselExampleAutoplaying" className="carousel slide mt-4" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <video className="d-block w-100" autoPlay loop muted style={{ borderRadius: '15px' }}>
+            <source src="\videos\INV@DER$.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="carousel-item">
+          <video className="d-block w-100" autoPlay loop muted style={{ borderRadius: '15px' }}>
+            <source src="\videos\INV@DER$.gif" type="video/gif" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="carousel-item">
+          <video className="d-block w-100" autoPlay loop muted style={{ borderRadius: '15px' }}>
+            <source src="path/to/your/video3.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;
